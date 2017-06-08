5f695828c70b1479a33a6b5e764984332595d140
chlee <chlee@gpmobile.co.kr>
2017-06-09 03:04:55 +0900

    .

--- cube01/cube01/GMetalView.swift
+++ cube01/cube01/GMetalView.swift
@@ -16,6 +16,10 @@ struct MBEVertex {
     var color : vector_float4
 }
 
+struct MBEUniforms {
+    var modelViewProjectionMatrix : matrix_float4x4
+}
+
 class GMetalView: UIView {
 
     /*
@@ -28,9 +32,15 @@ class GMetalView: UIView {
     
     var device : MTLDevice?
     var vertexBuffer: MTLBuffer?
+    var indexBuffer: MTLBuffer?
     var pipeline : MTLRenderPipelineState?
+    var depthStencilState : MTLDepthStencilState?
+    var depthTexture : MTLTexture?
     var commandQueue : MTLCommandQueue?
     var displayLink : CADisplayLink?
+    var elapsedTime : Float = 0
+    var rotationX : Float = 0
+    var rotationY : Float = 0
     
     override class var layerClass: Swift.AnyClass {
         return CAMetalLayer.self
@@ -66,12 +76,36 @@ class GMetalView: UIView {
     }
     
     @objc func displayLinkDidFire() {
+        let duration : Float = 1.0 / 60.0
+        elapsedTime += duration
+        self.rotationX += duration * Float(Double.pi / 2);
+        self.rotationY += duration * Float(Double.pi / 3);
         redraw()
     }
     
     func redraw() {
+        
         let drawable = self.metalLayer?.nextDrawable()
         let texture = drawable?.texture
+        
+        let scaleFactor = sin(5 * self.elapsedTime) * 0.25 + 1
+        let xAxis = vector_float3(1, 0, 0)
+        let yAxis = vector_float3(0, 1, 0)
+        let xRot = matrix_float4x4_rotation(axis: xAxis, angle: rotationX)
+        let yRot = matrix_float4x4_rotation(axis: yAxis, angle: rotationY)
+        let scale = matrix_float4x4_uniform_scale(scale: scaleFactor)
+        let modelMatrix = matrix_multiply(matrix_multiply(xRot, yRot), scale)
+        
+        let cameraTranslation = vector_float3(0, 0, -5)
+        let viewMatrix = matrix_float4x4_translation(t: cameraTranslation)
+        
+        let drawableSize = self.metalLayer!.drawableSize
+        let aspect = Float(drawableSize.width / drawableSize.height)
+        let fov = Float(2 * Double.pi) / 5
+        let near : Float = 1
+        let far : Float = 100
+        let projectionMatrix = matrix_float4x4_perspective(aspect: aspect, fovy: fov, near: near, far: far)
+        
         
         let passDescriptor = MTLRenderPassDescriptor()
         passDescriptor.colorAttachments[0].texture = texture
@@ -79,11 +113,27 @@ class GMetalView: UIView {
         passDescriptor.colorAttachments[0].storeAction = .store
         passDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 1, green: 1, blue: 0, alpha: 1)
         
+        makeDepthTexture()
+        passDescriptor.depthAttachment.texture = self.depthTexture
+        passDescriptor.depthAttachment.clearDepth = 1.0
+        passDescriptor.depthAttachment.loadAction = .clear
+        passDescriptor.depthAttachment.storeAction = .dontCare
+        
         let commandBuffer = commandQueue?.makeCommandBuffer()
         let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: passDescriptor)
         commandEncoder?.setRenderPipelineState(self.pipeline!)
+        commandEncoder?.setDepthStencilState(self.depthStencilState)
+        commandEncoder?.setFrontFacing(.counterClockwise)
+        commandEncoder?.setCullMode(.back)
         commandEncoder?.setVertexBuffer(self.vertexBuffer, offset: 0, index: 0)
-        commandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3)
+        
+        var uniforms = MBEUniforms(modelViewProjectionMatrix: matrix_multiply(matrix_multiply(projectionMatrix, viewMatrix), modelMatrix))
+        commandEncoder?.setVertexBytes(&uniforms,
+                                      length: MemoryLayout<MBEUniforms>.stride,
+                                      index: 1)
+        
+        commandEncoder?.drawIndexedPrimitives(type: .triangle, indexCount: self.indexBuffer!.length / MemoryLayout<UInt16>.size, indexType: .uint16, indexBuffer: self.indexBuffer!, indexBufferOffset: 0)
+        
         commandEncoder?.endEncoding()
         
         commandBuffer?.present(drawable!)
@@ -97,13 +147,29 @@ class GMetalView: UIView {
     }
     
     func makeBuffers() {
+        
         let vertices = [
-            MBEVertex(position: vector_float4(0, 0.5, 0, 1), color: vector_float4(1, 0, 0, 1)),
-            MBEVertex(position: vector_float4(-0.5, -0.5, 0, 1), color: vector_float4(0, 1, 0, 1)),
-            MBEVertex(position: vector_float4(0.5, -0.5, 0, 1), color: vector_float4(0, 0, 1, 1))
+            MBEVertex(position: vector_float4(-1, 1, 1, 1), color: vector_float4(0, 1, 1, 1)),
+            MBEVertex(position: vector_float4(-1, -1, 1, 1), color: vector_float4(0, 0, 1, 1)),
+            MBEVertex(position: vector_float4(1, -1, 1, 1), color: vector_float4(1, 0, 1, 1)),
+            MBEVertex(position: vector_float4(1, 1, 1, 1), color: vector_float4(1, 1, 1, 1)),
+            MBEVertex(position: vector_float4(-1, 1, -1, 1), color: vector_float4(0, 1, 0, 1)),
+            MBEVertex(position: vector_float4(-1, -1, -1, 1), color: vector_float4(0, 0, 0, 1)),
+            MBEVertex(position: vector_float4(1, -1, -1, 1), color: vector_float4(1, 0, 0, 1)),
+            MBEVertex(position: vector_float4(1, 1, -1, 1), color: vector_float4(1, 1, 0, 1))
         ]
         
         self.vertexBuffer = device?.makeBuffer(bytes: vertices, length: vertices.count * MemoryLayout<MBEVertex>.size, options: [])
+        
+        let indices : [UInt16] = [
+            3, 2, 6, 6, 7, 3,
+            4, 5, 1, 1, 0, 4,
+            4, 0, 3, 3, 7, 4,
+            1, 5, 6, 6, 2, 1,
+            0, 1, 2, 2, 3, 0,
+            7, 6, 5, 5, 4, 7
+        ]
+        self.indexBuffer = device?.makeBuffer(bytes: indices, length: indices.count * MemoryLayout<UInt16>.size, options: [])
     }
     
     func makePipeline() {
@@ -115,8 +181,97 @@ class GMetalView: UIView {
         pipelineDescriptor.vertexFunction = vertexFunc
         pipelineDescriptor.fragmentFunction = fragmentFunc
         pipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        pipelineDescriptor.depthAttachmentPixelFormat = .depth32Float
+        
+        let depthStencilDescriptor = MTLDepthStencilDescriptor()
+        depthStencilDescriptor.depthCompareFunction = .less
+        depthStencilDescriptor.isDepthWriteEnabled = true
+        self.depthStencilState = self.device?.makeDepthStencilState(descriptor: depthStencilDescriptor)
         
         self.pipeline = try? self.device!.makeRenderPipelineState(descriptor: pipelineDescriptor)
+        
         self.commandQueue = self.device?.makeCommandQueue()
+    }
+    
+    func makeDepthTexture() {
+        let drawableSize = self.metalLayer!.drawableSize
+        
+        if let texture = self.depthTexture {
+            if Int(drawableSize.width) == texture.width && Int(drawableSize.height) == texture.height {
+                return
+            }
+        }
+        let desc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .depth32Float, width: Int(drawableSize.width), height: Int(drawableSize.height), mipmapped: false)
+        desc.usage = .renderTarget
+        depthTexture = self.device?.makeTexture(descriptor: desc)
+    }
+    
+    
+    func matrix_float4x4_translation(t:vector_float3) -> matrix_float4x4 {
+        let X = vector_float4( 1, 0, 0, 0 )
+        let Y = vector_float4(0, 1, 0, 0 )
+        let Z = vector_float4( 0, 0, 1, 0 )
+        let W = vector_float4(t.x, t.y, t.z, 1 )
+        
+        let mat = matrix_float4x4(columns:( X, Y, Z, W ))
+        return mat
+    }
+    
+    func matrix_float4x4_uniform_scale(scale:Float) -> matrix_float4x4 {
+        let X = vector_float4( scale, 0, 0, 0 )
+        let Y = vector_float4( 0, scale, 0, 0 )
+        let Z = vector_float4( 0, 0, scale, 0 )
+        let W = vector_float4( 0, 0, 0, 1 )
+        
+        let mat = matrix_float4x4(columns:( X, Y, Z, W ))
+        return mat
+    }
+    
+    func matrix_float4x4_rotation(axis:vector_float3, angle:Float) -> matrix_float4x4 {
+        let c = cos(angle)
+        let s = sin(angle)
+        
+        var X = vector_float4()
+        X.x = axis.x * axis.x + (1 - axis.x * axis.x) * c;
+        X.y = axis.x * axis.y * (1 - c) - axis.z * s;
+        X.z = axis.x * axis.z * (1 - c) + axis.y * s;
+        X.w = 0.0;
+        
+        var Y = vector_float4()
+        Y.x = axis.x * axis.y * (1 - c) + axis.z * s;
+        Y.y = axis.y * axis.y + (1 - axis.y * axis.y) * c;
+        Y.z = axis.y * axis.z * (1 - c) - axis.x * s;
+        Y.w = 0.0;
+        
+        var Z = vector_float4()
+        Z.x = axis.x * axis.z * (1 - c) - axis.y * s;
+        Z.y = axis.y * axis.z * (1 - c) + axis.x * s;
+        Z.z = axis.z * axis.z + (1 - axis.z * axis.z) * c;
+        Z.w = 0.0;
+        
+        var W = vector_float4()
+        W.x = 0.0;
+        W.y = 0.0;
+        W.z = 0.0;
+        W.w = 1.0;
+        
+        let mat = matrix_float4x4(columns:( X, Y, Z, W ))
+        return mat
+    }
+    
+    func matrix_float4x4_perspective(aspect:Float, fovy:Float, near:Float, far:Float) -> matrix_float4x4 {
+        let yScale = 1 / tan(fovy * 0.5);
+        let xScale = yScale / aspect;
+        let zRange = far - near;
+        let zScale = -(far + near) / zRange;
+        let wzScale = -2 * far * near / zRange;
+        
+        let P = vector_float4( xScale, 0, 0, 0 )
+        let Q = vector_float4( 0, yScale, 0, 0 )
+        let R = vector_float4( 0, 0, zScale, -1 )
+        let S = vector_float4( 0, 0, wzScale, 0 )
+        
+        let mat = matrix_float4x4(columns:( P, Q, R, S ))
+        return mat
     }
 }
--- cube01/cube01/shaders.metal
+++ cube01/cube01/shaders.metal
@@ -14,8 +14,18 @@ struct Vertex {
     float4 color;
 };
 
-vertex Vertex vertex_main(device Vertex* vertices[[buffer(0)]], uint vid[[vertex_id]]) {
-    return vertices[vid];
+struct Uniforms {
+    float4x4 modelViewProjectionMatrix;
+};
+
+vertex Vertex vertex_main(device Vertex* vertices[[buffer(0)]],
+                          constant Uniforms *uniforms[[buffer(1)]],
+                          uint vid[[vertex_id]]) {
+    
+    Vertex out;
+    out.position = uniforms->modelViewProjectionMatrix * vertices[vid].position;
+    out.color = vertices[vid].color;
+    return out;
 }
 
 fragment float4 fragment_main(Vertex inVertex[[stage_in]]) {
