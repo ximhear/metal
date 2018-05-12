68eda3ecfdf22daef3fd9b2cac2ec05507984d9c
chlee <chlee@gpmobile.co.kr>
2018-05-12 09:56:45 +0900

    .

---
+++ imageprocessing01/imageprocessing01/NSImage-extension.swift
@@ -0,0 +1,48 @@
+//
+//  NSImage-extension.swift
+//  imageprocessing01
+//
+//  Created by LEE CHUL HYUN on 5/11/18.
+//  Copyright © 2018 LEE CHUL HYUN. All rights reserved.
+//
+
+import Foundation
+import AppKit
+import Metal
+
+extension NSImage {
+    
+    convenience init?(texture: MTLTexture?) {
+        
+        guard let texture = texture else {
+            return nil
+        }
+        let imageSize = CGSize.init(width: texture.width, height: texture.height)
+        let imageByteCount = Int(imageSize.width * imageSize.height * 4)
+        let imageBytes = UnsafeMutablePointer<UInt8>.allocate(capacity: imageByteCount)
+        let bytesPerRow = UInt(imageSize.width) * 4
+        let region = MTLRegionMake2D(0, 0, Int(imageSize.width), Int(imageSize.height))
+        texture.getBytes(UnsafeMutableRawPointer.init(imageBytes), bytesPerRow: Int(bytesPerRow), from: region, mipmapLevel: 0)
+        guard let data = CFDataCreate(nil, imageBytes, imageByteCount) else { return nil }
+        let provider = CGDataProvider.init(data: data)
+        let bitsPerComponent = 8
+        let bitsPerPixel = 32
+        let space = CGColorSpaceCreateDeviceRGB()
+        let renderingIntent: CGColorRenderingIntent = .defaultIntent
+
+        let imageRef = CGImage.init(width: Int(imageSize.width),
+                                    height: Int(imageSize.height),
+                                    bitsPerComponent: bitsPerComponent,
+                                    bitsPerPixel: bitsPerPixel,
+                                    bytesPerRow: Int(bytesPerRow),
+                                    space: space,
+                                    bitmapInfo: CGBitmapInfo(rawValue: CGBitmapInfo.byteOrder32Big.rawValue | CGImageAlphaInfo.premultipliedLast.rawValue),
+                                    provider: provider!,
+                                    decode: nil,
+                                    shouldInterpolate: false,
+                                    intent: renderingIntent)
+        imageBytes.deallocate()
+        
+        self.init(cgImage: imageRef!, size: NSSize.init(width: imageSize.width, height: imageSize.height))
+    }
+}
